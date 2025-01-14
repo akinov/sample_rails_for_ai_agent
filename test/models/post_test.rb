a7b06450c2f8d4780f5e04de857b27d1393d37c5
akinov <ayumoto10@gmail.com>
2025-01-14 20:52:13 +0900

    [cline] Add method to calculate time difference in Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'hours_difference with default datetime' do
+    post = Post.create(created_at: 2.hours.ago)
+    assert_in_delta 2.0, post.hours_difference, 0.01
+  end
+
+  test 'hours_difference with specified datetime' do
+    post = Post.create(created_at: 5.hours.ago)
+    specified_time = 3.hours.ago
+    assert_in_delta 2.0, post.hours_difference(specified_time), 0.01
+  end
 end
