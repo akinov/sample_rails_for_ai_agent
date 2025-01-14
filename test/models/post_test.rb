2cab2b4aee89a2ef365ec5b3fd1fc66d8b1041e5
akinov <ayumoto10@gmail.com>
2025-01-14 20:40:30 +0900

    [cline] Add method to return title or content preview

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'title_or_content_preview returns title if present' do
+    post = Post.new(title: 'Sample Title', content: 'Sample content that is longer than twenty characters.')
+    assert_equal 'Sample Title', post.title_or_content_preview
+  end
+
+  test 'title_or_content_preview returns first 20 characters of content if title is absent' do
+    post = Post.new(content: 'Sample content that is longer than twenty characters.')
+    assert_equal 'Sample content that ', post.title_or_content_preview
+  end
+
+  test 'title_or_content_preview returns empty string if both title and content are absent' do
+    post = Post.new
+    assert_equal '', post.title_or_content_preview
+  end
 end
